6a9a9ee8585612509db0b5f82b36c235a5250715
Nithishkumar <nithishcse2021@gmail.com>
2025-05-03 16:25:56 +0530

    Added Game Code

---
+++ BrickBreakerGame/GameMain.java
@@ -0,0 +1,17 @@
+package BrickBreakerGame;
+
+import javax.swing.JFrame;
+
+public class GameMain {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+
+        frame.setBounds(10, 10, 700, 600);
+        frame.setTitle("Brick Breaker Game");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePlay);
+    }
+}
